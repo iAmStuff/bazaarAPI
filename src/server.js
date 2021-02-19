0f54664a5c721f5edce8a63fc01f3c12bab2ee45
Quentin Sothern <quentinsothern@developersinstitute.co.nz>
2021-02-19 16:37:13 +1300

    GET /books route functional

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
 import express from 'express';
+import { Mongoose } from 'mongoose';
+import { connect, disconnect } from './database';
+import Books from './models/bookModel';
+
+connect();
+const server = express();
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+server.get('/books', async (req, res) => {
+  try {
+    const books = await Books.find();
+    console.log(books);
+    return res.json(books);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send(e);
+  }
+});
